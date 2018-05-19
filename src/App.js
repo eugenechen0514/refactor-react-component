563d3c694ca41b9cf89d4cee8aee95ae37eef428
Unknown <yuchiehchen@outlook.com>
2018-05-19 17:59:37 +0800

    initial project

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginBox />
+        <LoginBox onClickButton={console.log} />
       </div>
     );
   }
--- src/LoginBox.js
+++ src/LoginBox.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class LoginBox extends Component {
     constructor(props) {
         super(props);
         this.__handleChangeUsername = this.__handleChangeUsername.bind(this);
         this.__handleChangePassword = this.__handleChangePassword.bind(this);
+        this.__handleClickButton = this.__handleClickButton.bind(this);
+
+        this.state = {
+            username: '',
+            password: '',
+        };
     }
-    state = {
-      username: '',
-      password: '',
-    };
 
     __handleChangeUsername(event) {
         const value = event.target.value;
@@ -25,6 +28,10 @@ class LoginBox extends Component {
         });
     }
 
+    __handleClickButton() {
+        const {onClickButton} = this.props;
+        onClickButton(this.state);
+    }
     render() {
         const {username, password} = this.state;
         return (
@@ -36,9 +43,17 @@ class LoginBox extends Component {
                 <div>
                     密碼: <input value={password} onChange={this.__handleChangePassword} />
                 </div>
+                <button onClick={this.__handleClickButton}>送出</button>
             </div>
         );
     }
 }
 
+LoginBox.propTypes = {
+    onClickButton: PropTypes.func,
+};
+
+LoginBox.defaultProps = {
+    onClickButton: () => {},
+};
 export default LoginBox;
